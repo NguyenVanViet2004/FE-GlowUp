87672db3fd84216a4b41b92a24adf5f5389f2533
tudev <tukieu954@gmail.com>
2024-10-11 13:51:28 +0700

    Create template Specialist

--- components/templates/SpecialistTemplate.tsx
+++ components/templates/SpecialistTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useColorScheme } from 'react-native';
 import { Input, ScrollView, Text, View, YStack, Avatar, Stack } from 'tamagui';
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
@@ -6,25 +6,28 @@ import Header from '~/components/molecules/Header';
 import getColors from '~/constants/Colors';
 import useTranslation from '~/hooks/useTranslation';
 
+import Specialist from '../organisms/Specialist';
+import DatePicker from '../molecules/Date';
 type User = {
   name: string;
-  imageUri: string;
+  image: string;
 };
 
 const users: User[] = [
-  { name: 'Ronald', imageUri: 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=' },
-  { name: 'Ronald', imageUri: 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=' },
-  { name: 'Ronald', imageUri: 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=' },
-  { name: 'Ronald', imageUri: 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=' },
+  { name: 'Ronald', image: require('~/assets/images/Ellipse13.png') },
+  { name: 'Ronald', image: require('~/assets/images/Ellipse13.png') },
+  { name: 'Ronald', image: require('~/assets/images/Ellipse13.png') },
+  { name: 'Ronald', image: require('~/assets/images/Ellipse13.png') },
 ];
 
 const SpecialistTemplate: FC = () => {
   const colorScheme = useColorScheme();
   const colors = getColors(colorScheme);
   const { t } = useTranslation();
+  
 
   return (
-    <View flexGrow={1}>
+    <View padding={20}>
       <Header
         backIcon={
           <FontAwesome5 name="chevron-left" size={20} color={colors.text} />
@@ -32,18 +35,10 @@ const SpecialistTemplate: FC = () => {
         title={t('header.title', 'hehe boy')}
         subtitle={t('header.subtitle', 'bôi')}
       />
-      <Stack flexDirection="row" alignItems="center" gap="$2">
-        {users.map((user, index) => (
-          <Stack key={index} alignItems="center">
-            <Avatar circular size={60} borderWidth={index === 0 ? 2 : 0} borderColor={index === 0 ? colors.text : 'transparent'}>
-              <Avatar.Image source={{ uri: user.imageUri }} />
-            </Avatar>
-            <Text>{user.name}</Text>
-          </Stack>
-        ))}
-      </Stack>
+      <Specialist/>
+      {/* <DatePicker/> */}
     </View>
   );
 };
 
-export default SpecialistTemplate;
+export default SpecialistTemplate;  
