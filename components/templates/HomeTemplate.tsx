79233e316c1a9770245db3b52dbce1b09c941004
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-11-15 23:28:53 +0700

    Fetch combo data

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -1,8 +1,8 @@
-import { Bell } from '@tamagui/lucide-icons'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet, useColorScheme } from 'react-native'
 import { View } from 'tamagui'
 
+import { request } from '~/apis/HttpClient'
 import ContentTitle from '~/components/atoms/ContentTitle'
 import Banner from '~/components/molecules/Banner'
 import LinearGradientBackground from '~/components/molecules/LinearGradientBackground'
@@ -12,61 +12,6 @@ import getColors from '~/constants/Colors'
 import { RADIUS_BUTTON } from '~/constants/Constants'
 import useTranslation from '~/hooks/useTranslation'
 import type Combo from '~/interfaces/Combo'
-
-const comboDemo: Combo[] = [
-  {
-    _id: 'combo1',
-    description: 'Combo này bao gồm các món ăn ngon nhất của chúng tôi.',
-    imageUrl: 'https://example.com/image1.jpg',
-    name: 'Combo Đặc Biệt 1',
-    price: 199.99,
-    steps: [
-      {
-        _id: 'step1',
-        description: 'Preparation of the first course',
-        duration: '30 phút',
-        imageUrl: 'https://example.com/step1.jpg',
-        name: 'Step 1',
-        price: 50
-      }
-    ]
-  },
-  {
-    _id: 'combo2',
-    description: 'Combo bao gồm một bữa ăn cho 2 người với giá ưu đãi.',
-    imageUrl: 'https://example.com/image2.jpg',
-    name: 'Combo Đặc Biệt 2',
-    price: 249.99,
-    steps: [
-      {
-        _id: 'step2',
-        description: 'Main course preparation',
-        duration: '45 phút',
-        imageUrl: 'https://example.com/step2.jpg',
-        name: 'Step 2',
-        price: 80
-      }
-    ]
-  },
-  {
-    _id: 'combo3',
-    description: 'Combo này được thiết kế để tiết kiệm chi phí cho bạn.',
-    imageUrl: 'https://example.com/image3.jpg',
-    name: 'Combo Tiết Kiệm',
-    price: 149.99,
-    steps: [
-      {
-        _id: 'step3',
-        description: 'Dessert preparation',
-        duration: null,
-        imageUrl: 'https://example.com/step3.jpg',
-        name: 'Step 3',
-        price: 60
-      }
-    ]
-  }
-]
-
 const MemoizedBanner = React.memo(Banner)
 const MemoizedListCombo = React.memo(ListCombo)
 const MemoizedSpecialComboCard = React.memo(SpecialComboCard)
@@ -76,6 +21,8 @@ const HomeTemplate = (): React.ReactElement => {
   const colors = getColors(useColorScheme())
   const isDarkMode = useColorScheme() === 'dark'
   const [selectCombo, setSelectCombo] = useState<Combo | null>(null)
+  const [combos, setCombos] = useState<Combo[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const backGroundColor = isDarkMode ? 'transparent' : colors.ghostWhite
 
@@ -83,16 +30,34 @@ const HomeTemplate = (): React.ReactElement => {
     setSelectCombo(combo)
   }
 
+  useEffect(() => {
+    const fetchCombos = async (): Promise<void> => {
+      try {
+        const response = await request.get<Combo[]>('combo')
+        console.log(response.success)
+        if (response.success) {
+          setCombos(response.result as [])
+        } else {
+          console.error('Failed to fetch combos:', response.message)
+        }
+      } catch (error) {
+        console.error('Error while fetching combos:', error)
+      } finally {
+        setIsLoading(false)
+      }
+      console.log(combos)
+    }
+
+    fetchCombos()
+      .catch(error => { console.error('Error while fetching combos:', error) })
+  }, [])
+
   return (
     <LinearGradientBackground>
       <SafeAreaView
         style={[styles.container, { backgroundColor: backGroundColor }]}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <View
-            flexDirection="row"
-            alignItems="center"
-            width={'100%'}>
-
+          <View flexDirection="row" alignItems="center" width={'100%'}>
             <View flex={5}>
               <ContentTitle title={t('screens.home.yourBeautyIsOurPride')} />
             </View>
@@ -103,39 +68,47 @@ const HomeTemplate = (): React.ReactElement => {
               borderWidth={1}
               padding={10}
               alignItems="flex-end"
-
-            >
-              <Bell color={isDarkMode ? colors.white : colors.black} />
-            </View>
+            />
           </View>
 
           <MemoizedBanner
             marginTop={15}
             img={require('~/assets/images/imgBanner.png')}
             nameCombo="Morning Special!"
-            percent="20" />
+            percent="20"
+          />
 
-          <MemoizedListCombo
-            onSelectCombo={(combo) => { handleSelectCombo(combo) }}
-            title={t('screens.home.combo')}
-            combo={comboDemo}
-            marginTop={32} />
+          {isLoading
+            ? (
+              <View>
+                <ContentTitle title={t('screens.home.loading')} />
+              </View>)
+            : (
+              <MemoizedListCombo
+                onSelectCombo={(combo) => { handleSelectCombo(combo) }}
+                title={t('screens.home.combo')}
+                combo={combos}
+                marginTop={32}
+              />)}
 
-          {selectCombo !== undefined && <MemoizedSpecialComboCard
-            marginTop={32}
-            nameCombo={selectCombo?.name ?? 'Default Combo Name'}
-            percent="58"
-            quantity="6"
-            star="4.7"
-            view="20" />}
-
+          {selectCombo !== null && (
+            <MemoizedSpecialComboCard
+              marginTop={32}
+              nameCombo={selectCombo.name ?? 'Default Combo Name'}
+              percent="58"
+              quantity="6"
+              star="4.7"
+              view="20"
+            />
+          )}
         </ScrollView>
       </SafeAreaView>
-    </LinearGradientBackground >
+    </LinearGradientBackground>
   )
 }
 
 export default HomeTemplate
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
