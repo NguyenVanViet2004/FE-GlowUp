7f40ea15f8ef3bc458b45479a207639ef66200e2
Truong <truongtdph40209@fpt.edu.vn>
2024-11-16 15:19:04 +0700

    Apply api login and sigup

--- components/templates/LoginTemplate.tsx
+++ components/templates/LoginTemplate.tsx
@@ -1,21 +1,75 @@
 import { useRouter } from 'expo-router'
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import { isNil } from 'lodash'
+import React, { useEffect, useState } from 'react'
+import { Alert, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView, View } from 'tamagui'
 
+import { request } from '~/apis/HttpClient'
 import ContentTitle from '~/components/atoms/ContentTitle'
 import InputForm from '~/components/molecules/InputForm'
 import LinearGradientBackground from '~/components/molecules/LinearGradientBackground'
 import TextWithLink from '~/components/molecules/TextWithLink'
+import useStorage from '~/hooks/useStorage'
 import useTranslation from '~/hooks/useTranslation'
 
 const LoginTemplate: React.FC = (): JSX.Element => {
+  const { setItem, setObjectItem } = useStorage<string | object>()
   const { t } = useTranslation()
   const router = useRouter()
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
-  const handleLogin = (): void => {
-    router.replace('/(tabs)/home')
+  const [phoneError, setPhoneError] = useState<string>('')
+  const [passwordError, setPasswordError] = useState<string>('')
+
+  const validateForm = (): void => {
+    const passwordError = password === '' ||
+    password === null
+      ? t('screens.login.emtyPassword')
+      : ''
+
+    const phoneError = phoneNumber === '' ||
+    phoneNumber === null
+      ? t('screens.login.emtyPhone')
+      : ''
+
+    setPasswordError(passwordError)
+    setPhoneError(phoneError)
+  }
+  useEffect(() => {
+    validateForm()
+  }, [phoneNumber, password])
+
+  const handleLogin = async (): Promise<void> => {
+    try {
+      validateForm()
+
+      if (isNil(phoneError) || isNil(passwordError)) {
+        return
+      }
+      const payload = {
+        password,
+        phone_number: phoneNumber
+      }
+      const res = await request.post('/auth/login', payload)
+
+      if (!isNil(res.access_token) && !isNil(res.refresh_token)) {
+        await setItem('accessToken', res.access_token as string)
+        await setItem('refreshToken', res.refresh_token as string)
+        await setObjectItem('userData', JSON.stringify(res.data))
+        router.replace('/(tabs)/home')
+      } else {
+        setPasswordError(t('screens.login.incorrectAccountOrPassword'))
+        setPhoneError(t('screens.login.incorrectAccountOrPassword'))
+        console.log(res.message)
+      }
+    } catch (err) {
+      Alert.alert(
+        t('screens.signUp.false'),
+        t('screens.signUp.accountCreatedFalse')
+      )
+    }
   }
 
   const redirectToSignUp = (): void => {
@@ -38,10 +92,16 @@ const LoginTemplate: React.FC = (): JSX.Element => {
               visibleInputWithIcons={false}
               visibleForgotPassword={true}
               visibleSpace={true}
-              onLoginPress={handleLogin}
+              onLoginPress={() => {
+                handleLogin().catch((err) => { console.error(err) })
+              }}
               onLoginGooglePress={() => {}}
               positiveButtonTitle={t('screens.login.signIn')}
               negativeButtonTitle={t('screens.login.signInWithGoogle')}
+              onChangePhoneText={setPhoneNumber}
+              onChangePasswordText={setPassword}
+              passwordError={passwordError}
+              phoneError={phoneError}
             />
           </View>
 
--- components/templates/SignUpTemplate.tsx
+++ components/templates/SignUpTemplate.tsx
@@ -1,21 +1,95 @@
 import { useRouter } from 'expo-router'
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import { isNil } from 'lodash'
+import React, { useEffect, useState } from 'react'
+import { Alert, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView, View } from 'tamagui'
 
+import { request } from '~/apis/HttpClient'
 import ContentTitle from '~/components/atoms/ContentTitle'
 import InputForm from '~/components/molecules/InputForm'
 import LinearGradientBackground from '~/components/molecules/LinearGradientBackground'
 import TextWithLink from '~/components/molecules/TextWithLink'
 import useTranslation from '~/hooks/useTranslation'
 
+const validatePhoneNumber = (phone: string): boolean => {
+  const regex = /^(0[3|5|7|8|9]{1})[0-9]{8}$/
+  return regex.test(phone)
+}
+
 const SignUpTemplate: React.FC = (): JSX.Element => {
   const { t } = useTranslation()
   const router = useRouter()
+  const [fullName, setFullName] = useState<string>('')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
 
-  const handleSignUp = (): void => {
-    router.replace('/(tabs)/home')
+  const [nameError, setNameError] = useState<string>('')
+  const [phoneError, setPhoneError] = useState<string>('')
+  const [passwordError, setPasswordError] = useState<string>('')
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>('')
+
+  const validateForm = (): void => {
+    const nameError = fullName === '' ||
+    fullName === null
+      ? t('screens.signUp.emtyName')
+      : ''
+    const passwordError = password.length >= 6
+      ? ''
+      : t('screens.signUp.emtyPassword')
+    const confirmPasswordError = password === confirmPassword
+      ? ''
+      : t('screens.signUp.passwordDoesNotMatch')
+    const phoneError = validatePhoneNumber(phoneNumber)
+      ? ''
+      : t('screens.signUp.checkPhone')
+
+    setPasswordError(passwordError)
+    setConfirmPasswordError(confirmPasswordError)
+    setPhoneError(phoneError)
+    setNameError(nameError)
+  }
+
+  useEffect(() => {
+    validateForm()
+  }, [fullName, phoneNumber, password, confirmPassword])
+
+  const handleSignUp = async (): Promise<void> => {
+    try {
+      validateForm()
+
+      if (
+        isNil(nameError) ||
+        isNil(phoneError) ||
+        isNil(passwordError) ||
+        isNil(confirmPasswordError)
+      ) {
+        return
+      }
+
+      const payload = {
+        full_name: fullName,
+        password,
+        phone_number: phoneNumber
+      }
+
+      const res = await request.post('/auth/register', payload)
+      if (res.success) {
+        router.replace('/authentication/Login')
+        Alert.alert(
+          t('screens.signUp.succes'),
+          t('screens.signUp.accountCreatedSuccessfully')
+        )
+      } else {
+        setPhoneError(res.message)
+      }
+    } catch (err) {
+      Alert.alert(
+        t('screens.signUp.false'),
+        t('screens.signUp.accountCreatedFalse')
+      )
+    }
   }
 
   const redirectToLogin = (): void => {
@@ -38,10 +112,20 @@ const SignUpTemplate: React.FC = (): JSX.Element => {
               visibleInputWithIcons={true}
               visibleForgotPassword={false}
               visibleSpace={false}
-              onLoginPress={handleSignUp}
-              onLoginGooglePress={handleSignUp}
+              onLoginPress={() => {
+                handleSignUp().catch(err => { console.log(err) })
+              }}
+              onLoginGooglePress={() => {}}
               positiveButtonTitle={t('screens.login.joinNow')}
               negativeButtonTitle={t('screens.signUp.joinWithGoogle')}
+              onChangeNameText={setFullName}
+              onChangePhoneText={setPhoneNumber}
+              onChangePasswordText={setPassword}
+              onChangeConfirmPassWordText={setConfirmPassword}
+              nameError={nameError}
+              phoneError={phoneError}
+              passwordError={passwordError}
+              confirmPasswordError={confirmPasswordError}
             />
           </View>
           <View marginTop={'18%'} >
