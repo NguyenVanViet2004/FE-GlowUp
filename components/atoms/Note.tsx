25b161585ad796ea0a46e1477cd237952f38dfb5
tudev <tukieu954@gmail.com>
2024-11-18 00:29:55 +0700

    Create component note

--- components/atoms/Note.tsx
+++ components/atoms/Note.tsx
@@ -1,22 +1,51 @@
-import { TamaguiProvider, Stack, Text, TextArea } from "tamagui";
+import { useState } from 'react'
+import { useColorScheme } from 'react-native'
+import { Stack, Text, TextArea } from 'tamagui'
 
-export default function NotesComponent() {
+import getColors from '~/constants/Colors'
+import { useAppFonts } from '~/hooks/useAppFonts'
+import useTranslation from '~/hooks/useTranslation'
+
+interface NotesComponentProps {
+  title?: string
+  placeholder?: string
+  initialValue?: string
+  onSave?: (value: string) => void
+  color?: string
+}
+
+export default function NotesComponent ({
+  title = 'specialist.notes',
+  placeholder = 'Type your notes here',
+  initialValue = ''
+}: NotesComponentProps): JSX.Element {
+  const { fonts } = useAppFonts()
+  const { t } = useTranslation()
+  const [notes, setNotes] = useState(initialValue)
+  const colors = getColors(useColorScheme())
+
   return (
     <Stack
       borderRadius={5}
-      width="90%" // Chiều rộng nhỏ hơn để tạo khoảng cách hai bên
-      alignSelf="center" // Căn giữa Stack trong màn hình
+      width="93%"
+      alignSelf="center"
+      shadowOpacity={0.1}
+      shadowRadius={5}
     >
-      <Text marginBottom={8}>Notes</Text>
+      <Text marginBottom={8} fontFamily={fonts.JetBrainsMonoBold}>
+        {t(title)}
+      </Text>
       <TextArea
-        placeholder="Type your notes here"
+        value={notes}
+        onChangeText={setNotes}
+        placeholder={placeholder}
         width="100%"
         padding={20}
-        backgroundColor="#f0f4f8"
+        backgroundColor={colors.lightGray}
         borderRadius={20}
         height={120}
         lineHeight={24}
       />
     </Stack>
-  );
+  )
 }
