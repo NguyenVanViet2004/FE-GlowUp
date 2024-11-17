b8fc1c86d15287a39f1717e787ea23b7e1637bbb
tudev <tukieu954@gmail.com>
2024-11-18 00:33:11 +0700

    Add API get stylist

--- components/organisms/Specialist.tsx
+++ components/organisms/Specialist.tsx
@@ -1,69 +1,108 @@
 import Feather from '@expo/vector-icons/Feather'
-import { fonts } from '@tamagui/config/v3'
-import React, { useState } from 'react'
-import { StyleSheet, useColorScheme } from 'react-native'
-import { Avatar, Stack, Text, View } from 'tamagui'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, TouchableOpacity, useColorScheme } from 'react-native'
+import { Avatar, Text, View } from 'tamagui'
 
+import { request } from '~/apis/HttpClient'
 import getColors from '~/constants/Colors'
-
-interface User {
-  name: string
-  image: string
-}
-
-const users: User[] = [
-  { image: require('~/assets/images/Ellipse13.png'), name: 'Ronald' },
-  { image: require('~/assets/images/Ellipse13.png'), name: 'Ronald' },
-  { image: require('~/assets/images/Ellipse13.png'), name: 'Ronald' },
-  { image: require('~/assets/images/Ellipse13.png'), name: 'Ronald' }
-]
+import { useAppFonts } from '~/hooks/useAppFonts'
+import useTranslation from '~/hooks/useTranslation'
+import type Stylist from '~/interfaces/Stylist'
 
 const Specialist: React.FC = () => {
   const colorScheme = useColorScheme()
   const colors = getColors(colorScheme)
+  const { fonts } = useAppFonts()
+  const { t } = useTranslation()
   const [selectedUserIndex, setSelectedUserIndex] =
    useState<number | null>(null)
+  const [users, setUsers] = useState<Stylist[]>([])
+
+  useEffect(() => {
+    const fetchStylists = async (): Promise<void> => {
+      try {
+        const response = await request.get<Stylist[]>('/stylist/')
+        if (response?.success &&
+          Array.isArray(response.data) &&
+           response.data.length > 0) {
+          setUsers(response.data)
+        } else {
+          console.error(
+            'Failed to fetch stylist data:',
+            response?.message || 'Invalid response structure'
+          )
+        }
+      } catch (error) {
+        console.error('Error fetching stylist data:', error)
+      }
+    }
+
+    void fetchStylists()
+  }, [])
 
   const handleAvatarPress = (index: number): void => {
     setSelectedUserIndex(index === selectedUserIndex ? null : index)
   }
 
+  const renderUser = ({
+    item,
+    index
+  }: {
+    item: Stylist
+    index: number
+  }): JSX.Element => (
+    <TouchableOpacity
+      onPress={() => {
+        handleAvatarPress(index)
+      }}
+      style={styles.avatarContainer}
+    >
+      <Avatar
+        circular
+        size={80}
+        borderWidth={index === selectedUserIndex ? 3 : 0}
+        borderColor={colors.labelButton}
+        style={index === selectedUserIndex ? styles.avatarSelected : undefined}
+      >
+        <Avatar.Image
+          source={{
+            uri: item.avatar?.trim() ?? 'https://via.placeholder.com/80' // Sử dụng nullish coalescing operator
+          }}
+        />
+        {index === selectedUserIndex && (
+          <Feather
+            name="check"
+            size={24}
+            color={colors.labelButton}
+            style={styles.checkIcon}
+          />)}
+      </Avatar>
+      <Text>{item.full_name || t('specialist.unknown')}</Text>
+    </TouchableOpacity>
+  )
+
   return (
-    <View padding={10}>
-      <Text >Special list</Text>
-      <Stack flexDirection="row" alignItems="center" gap="$2" marginTop="$3.5">
-        {users.map((user, index) => (
-          <Stack
-            key={index}
-            alignItems="center"
-            onPress={() => { handleAvatarPress(index) }}
-          >
-            <Avatar
-              circular
-              size={80}
-              borderWidth={index === selectedUserIndex ? 3 : 0}
-              borderColor= {colors.labelButton}
-              style={index === selectedUserIndex && styles.avatarSelected}
-            >
-              <Avatar.Image source={{ uri: user.image }} />
-              {index === selectedUserIndex && (
-                <Feather
-                  name="check"
-                  size={24}
-                  color={colors.labelButton}
-                  style={styles.checkIcon}
-                />
-              )}
-            </Avatar>
-            <Text>{user.name}</Text>
-          </Stack>
-        ))}
-      </Stack>
+    <View padding={14}>
+      <Text fontFamily={fonts.JetBrainsMonoBold}>
+        {t('specialist.speciaList')}
+      </Text>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id?.toString() || Math.random().toString()} // Xử lý khi id null
+        renderItem={renderUser}
+        horizontal
+        contentContainerStyle={styles.listContainer}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  avatarContainer: {
+    alignItems: 'center',
+    padding: 10 // Chuyển inline style sang đây
+  },
   avatarSelected: {
     opacity: 0.5
   },
@@ -72,6 +111,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '50%',
     transform: [{ translateX: -12 }, { translateY: -12 }]
+  },
+  listContainer: {
+    marginTop: 14,
+    paddingHorizontal: 10
   }
 })
 
