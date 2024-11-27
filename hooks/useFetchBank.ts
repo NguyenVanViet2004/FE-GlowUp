91911296e5099f32c6da6653fb5ccc2fd63da23e
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-11-27 22:48:30 +0700

    Custom hook to fetch bank data

---
+++ hooks/useFetchBank.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+import { request } from '~/apis/HttpClient'
+import type Bank from '~/interfaces/Bank'
+
+interface UseFetchComboReturn {
+  bank: Bank[]
+  isLoading: boolean
+}
+
+const useFetchBank = (): UseFetchComboReturn => {
+  const [bank, setBank] = useState<Bank[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchBank = async (): Promise<void> => {
+      try {
+        setIsLoading(true)
+        const response =
+        await request.get<Bank[]>('payment/bank-list')
+        if (response !== null) {
+          console.log('BANK: ', bank)
+          setBank(response as Bank[])
+        }
+      } catch (err: any) {
+        console.error('Error fetching banks:', err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchBank().catch((err) => { console.error('Unexpected error:', err) })
+  }, [])
+
+  return { bank, isLoading }
+}
+
+export default useFetchBank
