06770fb8b3e7b7b719b2970e1cc9e54736a18e98
Nguyen Van Viet <nviet03052004@gmail.com>
2024-12-03 12:22:06 +0700

    ✨ Create useColorScheme hook to get and update theme value

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,39 +1,45 @@
-import React from "react"
-import { isNil } from "lodash"
+import { isNil } from 'lodash'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import useStorage from "~/hooks/useStorage"
+import { setColorScheme } from '~/features/colorSlice'
+import useStorage from '~/hooks/useStorage'
+import { type RootState } from '~/redux/store'
 
 interface props {
-  colorScheme: "light" | "dark"
-  setTheme: (scheme: "light" | "dark") => Promise<void>
+  colorScheme: 'light' | 'dark'
+  setTheme: (scheme: 'light' | 'dark') => Promise<void>
 }
 
 export const useColorScheme = (): props => {
   const { getItem, setItem } = useStorage()
-  const [colorScheme, setColorScheme] = React.useState<"light" | "dark">(
-    "light"
+  const dispatch = useDispatch()
+  const colorScheme = useSelector(
+    (state: RootState) => state.colorScheme.colorScheme
   )
 
-  const setTheme = async (scheme: "light" | "dark"): Promise<void> => {
-    await setItem("colorScheme", scheme)
-      .then(() => setColorScheme(scheme))
-      .catch((err) => console.error(err))
+  const setTheme = async (scheme: 'light' | 'dark'): Promise<void> => {
+    await setItem('colorScheme', scheme)
+      .then(() => dispatch(setColorScheme(scheme)))
+      .catch((err) => {
+        console.error(err)
+      }).catch(e => { console.error(e) })
   }
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     ;(async () => {
-      const savedColorScheme = await getItem("colorScheme")
+      const savedColorScheme = await getItem('colorScheme')
       if (
         !isNil(savedColorScheme) &&
-        (savedColorScheme === "light" || savedColorScheme === "dark")
+        (savedColorScheme === 'light' || savedColorScheme === 'dark')
       ) {
-        setColorScheme(savedColorScheme)
+        dispatch(setColorScheme(savedColorScheme))
       }
-    })(),
-      []
+    })().catch(e => { console.error(e) }),
+    []
   })
   return {
     colorScheme,
-    setTheme,
+    setTheme
   }
 }
