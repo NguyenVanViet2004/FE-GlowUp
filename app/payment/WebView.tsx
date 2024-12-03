672f6b075f4bc5903db4e9431babccd6f96c5f44
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-12-03 13:21:29 +0700

    Create WebviewTemplate

---
+++ app/payment/WebView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import WebviewTemplate from '~/components/templates/WebviewTemplate'
+
+const WebView = (): React.ReactElement => <WebviewTemplate/>
+
+export default WebView
